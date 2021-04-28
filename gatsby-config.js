f239cc2d0d1936a709ec2da9995d565d7803a9a7
Ryo Konishi <ryoestrella@gmail.com>
2021-04-29 00:27:57 +0200

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: `preview.contentful.com`
-        
+        host: "preview.contentful.com"
       },
     },
     {
